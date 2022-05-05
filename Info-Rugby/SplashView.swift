b9dfd3f5b8c1a930e8b8bc48dc7538a28b0bcf8a
Seik Oh <seikoh@naver.com>
2022-05-05 15:38:47 +0900

    [RESTRUCT] SplashView

--- Info-Rugby/SplashView.swift
+++ Info-Rugby/SplashView.swift
@@ -10,17 +10,19 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         ZStack{
-            VStack{
-                Image("olympic-rugby")
-                Text("Info Rugby")
-                    .font(.title2)
-                    //.foregroundColor(Color.logoGreen)
-                    .fontWeight(.heavy)
-                    .multilineTextAlignment(.center)
-            }
+        VStack{
+            Image("olympic-rugby")
+                .resizable()
+                .frame(width: 250, height: 270, alignment: .center)
+            Text("Infor Rugby")
+                .font(.title)
+                .fontWeight(.bold)
+
+        }
             VStack{
                 Spacer()
-                Text("V 1.0.0")
+                Text("V.1.0.0")
+                    .font(.system(size: 18))
                     .fontWeight(.bold)
             }
         }
